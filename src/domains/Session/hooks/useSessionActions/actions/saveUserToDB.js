7ca061aa1ab9e371b0d324972f209948d09d577f
olexandermushtyn <mushtynolexander@gmail.com>
2022-07-20 16:18:18 +0300

    Update saveUserToDB function

--- src/domains/Session/hooks/useSessionActions/actions/saveUserToDB.js
+++ src/domains/Session/hooks/useSessionActions/actions/saveUserToDB.js
@@ -7,15 +7,28 @@ const { USERS } = COLLECTIONS
  * Save the user's data to the database.
  * @returns A promise that resolves to the user object.
  */
-const saveUserToDB = ({ id, email, avatarUrl, agreement, gdpr, onError }) => {
+const saveUserToDB = ({
+  id,
+  email,
+  avatarUrl,
+  agreement,
+  gdpr,
+  additionalData,
+  onError
+}) => {
   const firestore = firebase.firestore()
   return firestore
     .collection(USERS)
     .doc(id)
     .set({
-      id,
+      _createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      _updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      _isUpdated: false,
+      _updatedBy: id,
+      _id: id,
       email,
       emailVerified: false,
+      ...additionalData,
       agreement,
       gdpr,
       avatarUrl
