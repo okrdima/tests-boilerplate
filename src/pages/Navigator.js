e7e3ffbf22182de193734839f11863352d9b7514
olexandermushtyn <mushtynolexander@gmail.com>
2022-07-20 17:24:08 +0300

    Delete useless router

--- src/pages/Navigator.js
+++ src/pages/Navigator.js
@@ -1,9 +1,4 @@
-import {
-  Redirect,
-  Route,
-  BrowserRouter as Router,
-  Switch
-} from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 import { App } from './App'
 import { Auth } from './Auth'
@@ -12,18 +7,16 @@ import { Service } from './Service'
 
 const Navigator = () => {
   return (
-    <Router>
-      <RoutesRedirect>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/auth" />
-          </Route>
-          <Route path="/auth/:path?" component={Auth} />
-          <Route path="/service/:path?" component={Service} />
-          <Route component={App} />
-        </Switch>
-      </RoutesRedirect>
-    </Router>
+    <RoutesRedirect>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/auth" />
+        </Route>
+        <Route path="/auth/:path?" component={Auth} />
+        <Route path="/service/:path?" component={Service} />
+        <Route component={App} />
+      </Switch>
+    </RoutesRedirect>
   )
 }
 
