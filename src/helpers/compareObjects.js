560272510eb7f079e1d392b8a768477bd7af6260
olexandermushtyn <mushtynolexander@gmail.com>
2022-09-12 17:17:28 +0300

    Fix comments

--- src/helpers/compareObjects.js
+++ src/helpers/compareObjects.js
@@ -1,13 +1,22 @@
 import _ from 'lodash'
 import normalizeObject from './normalizeObject'
 
-const compareObjects = (a, b) => {
+/**
+ * It compares two objects and returns an object with the keys that are different and the values that
+ * are different
+ * @param firstObject - The first object to compare.
+ * @param secondObject - The object that you want to compare against.
+ */
+const compareObjects = (firstObject, secondObject) => {
   const compareObject = _.reduce(
-    a,
+    firstObject,
     (result, value, key) =>
-      _.isEqual(value, b[key])
+      _.isEqual(value, secondObject[key])
         ? result
-        : { ...result, [key]: { prev: a[key], next: b[key] } },
+        : {
+            ...result,
+            [key]: { prev: firstObject[key], next: secondObject[key] }
+          },
     {}
   )
 
