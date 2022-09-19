fb0adc51da0e3752e63166aa0e5ff4eddca4ee5f
Arthur-Pochinok-MENTA <arthur.pochinok@senseteq.io>
2022-09-19 13:23:45 +0200

    [SQELETON-WEB-APP] [SWE-3983] [MINOR] eslint was fixed by config change + installing of extra package. #done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react-svg/recommended'
-  ],
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'react-svg'],
+  extends: ['react-app', 'prettier'],
+  root: true,
+  // parser: 'babel-eslint',
+  plugins: ['import', 'babel', 'react', 'react-hooks', 'prettier'],
   settings: {
     react: {
-      version: 'detect'
+      version: '17.0.2'
     },
     'import/resolver': {
       node: {
@@ -19,20 +13,28 @@ module.exports = {
       }
     }
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
   rules: {
-    'no-undef': 2,
-    'no-unsafe-optional-chaining': 1,
-    'no-console': 'warn',
-    'no-eval': 'error',
-    'no-unused-vars': 'warn',
-    'import/first': 'error',
+    semi: [2, 'never'],
+    // 'no-console': 'error',
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': 0,
+    'import/no-named-as-default': 0,
+    'no-return-await': 2,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'react/prop-types': 1
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        semi: false,
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false
+      }
+    ]
   }
 }
